7227ac39a2df1781d3972dacd8ad10c83d77f49b
Jordan Martin <jdordan23@gmail.com>
2018-08-10 03:44:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function logShout(string) {
   console.log(string.toUpperCase())
   return string.toUpperCase()
 }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+  return string.toLowerCase()
+}
