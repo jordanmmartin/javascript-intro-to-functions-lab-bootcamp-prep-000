c6b8715d47c21781cd1d5476bdc2ae815d2bf89f
Jordan Martin <jdordan23@gmail.com>
2018-08-10 03:52:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,7 @@ function sayHiToGrandma(string) {
     else if (string === uppercase) {
         return "YES INDEED!"
     }
-    else if (string === )
+    else if (string === "I love you, Grandma.") {
+        return 
+    }
 }
